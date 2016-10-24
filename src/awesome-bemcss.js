b77ec3250e3cd94213a988e48de672f29ffb2a22
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2016-10-24 15:08:25 +0700

    add tabs vertical and horizontal

--- src/awesome-bemcss.js
+++ src/awesome-bemcss.js
@@ -3,16 +3,47 @@
 /*
 TABS
 */
-var initTabsEvent = (function(){
-	var item = $('.tabs__item');
+function setActiveTab(target){
+    var tabsActive = $(".tabs").find("[data-target='" + target + "']");
 
-    item.on('click', function(){
-        item.removeClass('tabs__item--active');
-        $(this).addClass('tabs__item--active');
-        window.location.hash = $(this).attr('data-target');
-        
+    $.each(tabsActive, function(i, el){
+        if($(el).hasClass('tabs__item')){
+            $(el).siblings().removeClass('tabs__item--active');
+            $(el).addClass('tabs__item--active');
+        }else if($(el).hasClass('tabs__itemv')){
+            $(el).siblings().removeClass('tabs__itemv--active');
+            $(el).addClass('tabs__itemv--active');
+        }        
+    }); 
+    
+    $('html, body').animate({
+        scrollTop: $(target).offset().top
+    }, 1000);
+}
+
+function initTab(tabClass, tabActiveClass, otherTabActiveClass){
+    var itemv = $('.' + tabClass);
+    itemv.on('click', function(){
+        var target = $(this).attr('data-target');
+        setActiveTab(target);
+
+        window.location.hash = target;
         return false;
     });
+}
+
+var initTabsEvent = (function(){
+
+    initTab('tabs__item', 'tabs__item--active', 
+            'tabs__itemv--active');
+
+    initTab('tabs__itemv', 'tabs__itemv--active', 
+            'tabs__item--active');
+
+    var urlHash = window.location.hash;
+    if (urlHash !== "") {
+        setActiveTab(urlHash);
+    }
 })();
 
 $(document).ready(function () {
